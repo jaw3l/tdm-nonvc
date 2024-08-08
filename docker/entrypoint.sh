b3aeb389cccbe8bb9d6092100e8185b5df11adc2
jaw3l <16852455+jaw3l@users.noreply.github.com>
2024-08-08 15:24:40 +0300

    added novnc support

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,8 +6,17 @@ WORKING_DIR="/usr/src/app"
 COOKIES_PATH="${WORKING_DIR}/cookies.jar"
 SETTINGS_PATH="${WORKING_DIR}/settings.json"
 
+# Create symlinks for data files
 ln -s "${DATA_DIR}/cookies.jar" "${COOKIES_PATH}"
 ln -s "${DATA_DIR}/settings.json" "${SETTINGS_PATH}"
+
+# Set default values for environment variables
+: "${DISPLAY_WIDTH:=1024}"
+: "${DISPLAY_HEIGHT:=768}"
+: "${DISPLAY_DEPTH:=16}"
+: "${VNC_PORT:=5900}"
+: "${NOVNC_PORT:=8080}"
+: "${LOG_LEVEL:=INFO}"
 
 wait_for_novnc() {
   sleep 5
@@ -35,6 +44,18 @@ convert_log_level_to_verbosity() {
 }
 
 VERBOSITY=$(convert_log_level_to_verbosity)
+
+# Start Xvfb with the specified resolution
+Xvfb :0 -screen 0 "${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x${DISPLAY_DEPTH}" &
+
+# Start FluxBox window manager
+fluxbox &
+
+# Start VNC server
+x11vnc -xkb -forever -nopw -display :0 -listen localhost &
+
+# Start noVNC
+/usr/share/novnc/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NOVNC_PORT &
 
 wait_for_novnc
 
